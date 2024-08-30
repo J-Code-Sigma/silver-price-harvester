9a222f4b247c30f7d96f37fc624c45ddc5f32ddc
jcode <seejay181@gmail.com>
2024-08-30 11:05:14 -0500

    port for prod

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const env = loadEnv(mode, process.cwd());
+
+  const PORT = `${env.PORT_NUM ?? '8080'}`;
+
+
+  return {
   server: {
     host: "::",
-    port: "8080",
+    port: PORT,
   },
   plugins: [react()],
   resolve: {
@@ -22,4 +31,5 @@ export default defineConfig({
       },
     ],
   },
+};
 });
